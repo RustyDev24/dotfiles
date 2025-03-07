880af9454684cb628fc7f5c61145a17a2da5682e
Rusty Dev <vedant.matanhelia@gmail.com>
2025-03-07 07:07:56 +0530

    feat: remove autoformat

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -10,10 +10,6 @@ return {
       typescriptreact = { "prettierd" },
       lua = { "stylua" },
     },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_format = "fallback",
-    },
   },
   keys = {
     {
